f7d9143d9ee65f16df52a45c45e093623247490c
doxxon <gavin.s.cheng@gmail.com>
2018-05-28 19:37:02 +0800

    build

--- src/js/index.js
+++ src/js/index.js
@@ -87,7 +87,7 @@ $( document ).ready(function() {
             $('#preload').hide();
 
             // disable login for testing
-            loadDataSheet(appData.credentials.norway.sheet_id, appData.googleSheetApiKey, cardData)
+            // loadDataSheet(appData.credentials.norway.sheet_id, appData.googleSheetApiKey, cardData)
         },
         error: function(xhr, status, error) {
             var err = eval("(" + xhr.responseText + ")");
